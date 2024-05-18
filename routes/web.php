0e1f25407fbe57b0fb6d2827f7657a4b9a7e3cae
Rafalotto <rafael_andrade74@hotmail.com>
2024-05-17 23:28:12 -0300

    Implementado breeze

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,37 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EnderecoController;
 use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::prefix('/Home')->group(function () {
     Route::get('/{slug}', [HomeController::class, 'index'])->name('home');
 });
 
-//Criar Controlador
-Route::get('/login', [EnderecoController::class,'index'])->name('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::prefix('/Endereco')->group(function(){
-  Route::get('/', [EnderecoController::class,'index'])->name('endereco');
-  Route::get('/create', [EnderecoController::class,'create'])->name('endereco.create');
-  Route::post('/store', [EnderecoController::class,'store'])->name('endereco.store');
-  Route::get('/edit/{endereco}', [EnderecoController::class,'edit'])->name('endereco.edit');
-  Route::put('/update/{endereco}', [EnderecoController::class,'update'])->name('endereco.update');
-  Route::delete('/destroy/{endereco}', [EnderecoController::class,'destroy'])->name('endereco.destroy');
+Route::middleware('auth')->group(function () {
+    Route::prefix('/Endereco')->group(function(){
+        Route::get('/', [EnderecoController::class,'index'])->name('endereco');
+        Route::get('/create', [EnderecoController::class,'create'])->name('endereco.create');
+        Route::post('/store', [EnderecoController::class,'store'])->name('endereco.store');
+        Route::get('/edit/{endereco}', [EnderecoController::class,'edit'])->name('endereco.edit');
+        Route::put('/update/{endereco}', [EnderecoController::class,'update'])->name('endereco.update');
+        Route::delete('/destroy/{endereco}', [EnderecoController::class,'destroy'])->name('endereco.destroy');
+    });
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
 
 Route::Fallback(function(){
     return redirect()->away(Env('APP_URL_FALLBACK'));
 });
+
+require __DIR__.'/auth.php';
